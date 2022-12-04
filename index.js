15e381ff8a07ba3e32c02f4c025078e0892a0a71
Abu khaled <mkhkatanany@gmail.com>
2022-12-05 01:45:59 +0200

    first commit

--- index.js
+++ index.js
@@ -2,16 +2,27 @@
 let myBtn = document.querySelector(".rendom"),
     hexa = document.querySelector("#text"),
     paint = document.querySelector(".colorBox");
-// the first event for random color
-myBtn.onclick =()=>{
+
+const randomColor = ()=>{
     let r = Math.random()*255
     let g = Math.random()*255
     let b = Math.random()*255
-    hexa.value=`#${parseInt(r).toString(16)}${parseInt(g).toString(16)}${parseInt(b).toString(16)}`
-    paint.style.backgroundColor=`#${parseInt(r).toString(16)}${parseInt(g).toString(16)}${parseInt(b).toString(16)}`
+    let hexaColor=`#${parseInt(r).toString(16)}${parseInt(g).toString(16)}${parseInt(b).toString(16)}`
+    hexa.value=hexaColor
+    paint.style.backgroundColor=hexaColor
 }
+// the first event for random color
+myBtn.addEventListener('click',randomColor)
 
 // the second event for input color 
-    hexa.addEventListener('keyup',()=>{
+hexa.addEventListener('keyup',()=>{
         paint.style.backgroundColor=hexa.value;
     })
+// the optional part 
+    var colorint;
+paint.addEventListener('mouseover',()=>{
+    colorint=setInterval(randomColor,500)}
+)
+paint.addEventListener('mouseout',()=>{
+    clearInterval(colorint)
+})
